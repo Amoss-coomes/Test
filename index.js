621c132f926d13999a984d01c55ee6302bdc1279
Amoss-coomes <63299716+Amoss-coomes@users.noreply.github.com>
2020-04-07 14:40:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let name;
+let age;
+let jobrole = "photographer"
+
+
+function personalData(name, age){
+    console.log(" Hello, I am " + name + " and I am " + age + " years old. " )
+
+}
+
+personalData("Abi", 26)
+personalData("Dean", 19)
+
+let numberOne; 
+let numberTwo;
+
+function addition(numberOne, numberTwo){
+    let total = numberOne + numberTwo
+    console.log(total)
+
+}
+
+addition(30, 50)
+addition(50, 40)
